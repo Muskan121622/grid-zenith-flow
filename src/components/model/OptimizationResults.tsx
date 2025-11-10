609b5737c1ad8faddb4ad10f606736660c158e6e
Your Name <muskana1216@gmail.com>
2025-11-10 22:21:55 +0530

    fixed backend url

--- src/components/model/OptimizationResults.tsx
+++ src/components/model/OptimizationResults.tsx
@@ -17,7 +17,7 @@ const OptimizationResults = () => {
     setError(null);
     
     try {
-      const response = await fetch('http://localhost:5000/api/optimize', {
+      const response = await fetch('https://grid-zenith-flow-2.onrender.com/api/optimize', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
